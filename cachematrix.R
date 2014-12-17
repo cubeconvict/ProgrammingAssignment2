5b21988c7b7eaf9e67a58fccf5cb27773a4dc89d
Toby Kraft <kraft.toby@gmail.com>
2014-12-17 15:37:07 -0800

    Currently functional using a small test matrix.  No performance reporting

--- cachematrix.R
+++ cachematrix.R
@@ -7,20 +7,22 @@
 ## compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
 ## This version created 2014-12 by https://github.com/cubeconvict
+## significant help from
+## https://class.coursera.org/rprog-016/forum/thread?thread_id=96
 
 
 # makeCacheMatrix: This function creates a special "matrix" object
 # that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix()) {
      m <- NULL
      set <- function(y) {
           x <<- y
           m <<- NULL
      }
-     get <- function() x
-     setinverse <- function(solve) m <<- solve
-     getinverse <- function() m
+     get <- function() {x}
+     setinverse <- function(solve) {m <<- solve}
+     getinverse <- function() {m}
      list(set = set, get = get,
           setinverse = setinverse,
           getinverse = getinverse)
@@ -35,27 +37,44 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-     returned.list <- makeCacheMatrix(x)
-     print(returned.list)
-     # test if the inverse has already been cached
-     # if there is no cached version, calculate one
-     if (NULL == NULL) {
-          print("Boo!")
 
+     ## Return a matrix that is the inverse of 'x'
+     m <- x$getinverse()
+     # test if the inverse has already been cached
+     # if there is a cached version, return it
+     if (!is.null(m)) {
+
+          return(m)
      }
-     # assume that a cached version exists and return it
+     # If there is no cached version, calculate it
      else {
-          return(returned.list$getinverse)
+          data <- x$get()
+          m <- solve(data, ...)
+          x$setinverse(m)
+          m
      }
 
 }
 
+## Unittest function is a simple way to run a test
+## with a simple canned matrix, saves typing while debugging
+
 unittest <- function() {
-     bigMatrix <- makeCacheMatrix(1:1000)
-     # now we have an object 'bigVec' of type list
-     biggerMatrix <- makeCacheMatrix(1:100000)
-     # 'biggerVec' of type list with 100,000 numbers
-     cacheSolve(bigMatrix)
-     cacheSolve(biggerMatrix)
+     littleMatrix <- matrix(c(-1, -2, 1, 1), 2, 2)
+     littleMatrixList <- makeCacheMatrix(littleMatrix)
+     print(littleMatrixList$getinverse())
+     cacheSolve(littleMatrixList)
+     print(littleMatrixList$get())
+     cacheSolve(littleMatrixList)
+     print(littleMatrixList$get())
+     print(littleMatrixList$getinverse())
+
+
+     # now we have an object 'bigMatrixList' of type list
+#      biggerMatrix <- matrix(1:100000, nrow=100, ncol=100)
+#      biggerMatrixList <- makeCacheMatrix(biggerMatrix)
+#      cacheSolve(biggerMatrixList)
+#      cacheSolve(biggerMatrixList)
+
+
 }
